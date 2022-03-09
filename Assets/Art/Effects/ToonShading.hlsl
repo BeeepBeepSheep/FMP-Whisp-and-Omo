db4f60b41250d23597e5b878b46e760962a9b88d
BeeepBeepSheep <jacoblueke@gmail.com>
2022-03-09 12:16:19 +0000

    Shaders added

--- Assets/Art/Effects/ToonShading.hlsl
+++ Assets/Art/Effects/ToonShading.hlsl
@@ -1,4 +1,4 @@
-void ToonShading_float(in float3 Normal, in float ToonRampSmoothness, in float3 ClipSpacePos, in float3 WorldPos, in float4 ToonRampTinting,
+﻿void ToonShading_float(in float3 Normal, in float ToonRampSmoothness, in float3 ClipSpacePos, in float3 WorldPos, in float4 ToonRampTinting,
 	in float ToonRampOffset, out float3 ToonRampOutput, out float3 Direction)
 {
 
