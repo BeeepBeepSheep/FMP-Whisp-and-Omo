ba15b162cbe157312454a336d3f95a1e451ba259
Jacob Lueke <jacoblueke@gmail.com>
2022-05-25 14:45:07 +0100

    Ready to final build. fixed; cannon collider, prison exit door colliders, crane crush colliders, credits spelling, clariified teliport in controls and tutorial.

---
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,42 @@
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        The_Adventure_of_Whisp_and_Omo,
+    }
+
+    public enum Platform
+    {
+        None,
+        Linux,
+        macOS,
+        PC,
+        WebGL,
+    }
+
+    public enum Architecture
+    {
+        None,
+        Linux_x64,
+        macOS,
+        Windows_x86,
+        Windows_x64,
+        WebGL,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static readonly System.DateTime buildDate = new System.DateTime(637890697137910009);
+    public const string version = "1.0.1";
+    public const ReleaseType releaseType = ReleaseType.The_Adventure_of_Whisp_and_Omo;
+    public const Platform platform = Platform.WebGL;
+    public const Architecture architecture = Architecture.WebGL;
+    public const Distribution distribution = Distribution.None;
+}
+
